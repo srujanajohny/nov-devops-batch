d877647401d9eaedfe1aa6ad75fcf05cd9d34d07
srujana <srujanajohny@gmail.com>
2022-11-21 01:34:02 -0500

    python practice

---
+++ code.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+b=int(2.0)
+print(b)
+#c='ABC'
+x=complex(13)
+print(x)
+"""e=bool()
+f=bool(0)
+g=bool(1)
+h=bool(5)
+i=bool(a)
+j=bool('')
+"""
+#a=bool()
+#print(a)
+
+
+      
+"""x = range(6)
+for n in x:
+    print(n)"""
+
+#l=[1,2,3,4,6,5,4,3,1]
+"""t=(4,7,8,9,9,9,8,7)
+s={4,6,8,9,8,7,6}
+print (l)
+print (t)"""
+#print (l)
+
+"""a=1+4j
+print(a)"""
+
+L=[1,4,6,2,9,6,5,0]
+print(L.sort())
+
+#L.sort(reverse=True)
+print(L)
+#L[7]=9
+#print(L)
+
+#s={12,5,6,2,12,6,7,5}
+#print(s)
+
+#s[2]=6
+#print(t)
+
+# d={1:2,3:"drt",5:8}
+# d[5]=9
+# print(d.items())
+
+
+
+
